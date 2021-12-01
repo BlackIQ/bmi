be61288722946348812f7414834aba065d5c1b79
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-01 12:10:11 +0330

    Flutter init

---
+++ README.md
@@ -0,0 +1,3 @@
+# bmicalculator
+
+Body Mass Index calculator.
---
+++ android/app/src/main/kotlin/ir/blackiq/bmi/bmicalculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package ir.blackiq.bmi.bmicalculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
