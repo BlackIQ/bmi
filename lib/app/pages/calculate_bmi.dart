2fbaf9cbfeb4974c85f029b9400aaa4b58288d04
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-01 16:50:21 +0330

    Done in back end for calculating

--- lib/app/pages/calculate_bmi.dart
+++ lib/app/pages/calculate_bmi.dart
@@ -1,3 +1,5 @@
+import 'package:bmicalculator/app/widgets/button/button.dart';
+import 'package:bmicalculator/app/widgets/field/field.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -22,10 +24,95 @@ class _bmiCalculateState extends State<bmiCalculate> {
     );
   }
 
+  final TextEditingController _height = TextEditingController();
+  final TextEditingController _weight = TextEditingController();
+
+  bool calculated = false;
+  String bmiCalculated = '';
+
+  void calculate() {
+    var x = int.parse(_height.text);
+    var y = int.parse(_weight.text);
+    var z = x * y;
+    setState(() {
+      calculated = true;
+      bmiCalculated = z.toString();
+      _height.clear();
+      _weight.clear();
+    });
+  }
+
   Widget _build() {
-    return Center(
-      child: Text(
-        'BMI',
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              'BMI calculator',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Body mass index (BMI) is a value derived from the mass (weight) and height of a person. The BMI is defined as the body mass divided by the square of the body height, and is expressed in units of kg/m2, resulting from mass in kilograms and height in metres.',
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            SizedBox(height: 25),
+            bmiField(
+              controller: _height,
+              label: 'Enter your height',
+              hint: 'Value should be in Meter',
+            ),
+            SizedBox(height: 15),
+            bmiField(
+              controller: _weight,
+              label: 'Enter your weight',
+              hint: 'Value should be in Kilogram',
+            ),
+            SizedBox(height: 15),
+            bmiSubmitButton(
+              submit: calculate,
+              text: 'Calculate',
+            ),
+            SizedBox(height: 50),
+            calculated
+                ? Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Your BMI is ${bmiCalculated}.',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.indigo,
+                          ),
+                        ),
+                        Text(
+                          'Open drawer and choose Range BMI. Enter your BMI to analyse the BMI.',
+                          style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.indigo,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                : Text(
+                    'To see your BMI, fill these blanks.',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.red,
+                    ),
+                  )
+          ],
+        ),
       ),
     );
   }
