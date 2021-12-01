abd170ead2a9ee918541dd933269e7c4cb9968a6
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-01 12:46:56 +0330

    Create common item for drawer listTile

---
+++ lib/app/widgets/drawer/items/drawer_items.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+class DrawerItemWidget extends StatelessWidget {
+  DrawerItemWidget({required this.text, required this.icon, required this.onTap});
+  final icon;
+  final text;
+  final onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Row(
+        children: [
+          Icon(
+            icon,
+            color: Colors.indigo,
+          ),
+          SizedBox(width: 25),
+          Text(
+            text,
+            style: TextStyle(
+              color: Colors.indigo,
+            ),
+          ),
+        ],
+      ),
+      onTap: () => onTap,
+    );
+  }
+}
