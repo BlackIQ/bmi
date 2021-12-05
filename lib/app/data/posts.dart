3915fb1c9c8ea18e53bb4ba587b5954b73d9027e
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-05 23:43:54 +0330

    Add new sites

---
+++ lib/app/data/posts.dart
@@ -0,0 +1,37 @@
+List<Map> links = [
+  {
+    'site': 'WHO',
+    'about': 'Which fruit is good for children?',
+    'url': 'https://google.com',
+  },
+  {
+    'site': 'Health Education',
+    'about': 'What kind of diet is better?',
+    'url': 'https://google.com',
+  },
+  {
+    'site': 'Health Education',
+    'about': 'What kind of diet is better?',
+    'url': 'https://google.com',
+  },
+  {
+    'site': 'Health Education',
+    'about': 'What kind of diet is better?',
+    'url': 'https://google.com',
+  },
+  {
+    'site': 'Health Education',
+    'about': 'What kind of diet is better?',
+    'url': 'https://google.com',
+  },
+  {
+    'site': 'Health Education',
+    'about': 'What kind of diet is better?',
+    'url': 'https://google.com',
+  },
+  {
+    'site': 'Health Education',
+    'about': 'What kind of diet is better?',
+    'url': 'https://google.com',
+  },
+];
---
+++ lib/app/data/sources.dart
@@ -0,0 +1,48 @@
+List<Map> links = [
+  {
+    'site': 'Wikipedia',
+    'about': 'Article about Body Mass Index',
+    'url': 'https://en.wikipedia.org/wiki/Body_mass_index',
+  },
+  {
+    'site': 'Wikipedia',
+    'about': 'Article about overweight',
+    'url': 'https://en.wikipedia.org/wiki/Overweight',
+  },
+  // After
+  {
+    'site': 'Wikipedia',
+    'about': 'Article about Underweight',
+    'url': 'https://en.wikipedia.org/wiki/Underweight',
+  },
+  {
+    'site': 'Wikipedia',
+    'about': 'Article about Obesity',
+    'url': 'https://en.wikipedia.org/wiki/Obesity',
+  },
+  {
+    'site': 'Medical News Today',
+    'about': 'How much should I weigh for my height and age?',
+    'url': 'https://www.medicalnewstoday.com/articles/323446',
+  },
+  {
+    'site': 'Centers for Disease Control and Prevention',
+    'about': 'Obesity and adults',
+    'url': 'https://www.cdc.gov/obesity/adult/defining.html',
+  },
+  {
+    'site': 'Mount Sinai',
+    'about': 'Overweight',
+    'url': 'https://www.mountsinai.org/health-library/symptoms/overweight',
+  },
+  {
+    'site': 'WHO',
+    'about': 'Obesity and overweight',
+    'url': 'https://www.who.int/news-room/fact-sheets/detail/obesity-and-overweight',
+  },
+  {
+    'site': 'NIDDK',
+    'about': 'Facts for Adult Overweight & Obesity',
+    'url': 'https://www.niddk.nih.gov/health-information/weight-management/adult-overweight-obesity/definition-facts',
+  },
+];
