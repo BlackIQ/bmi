d3446916a11d119eac7e38de4d3772ca1347162d
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-06 11:45:24 +0330

    Fix icons in trading

--- lib/app/pages/sources.dart
+++ lib/app/pages/sources.dart
@@ -2,6 +2,7 @@ import 'package:bmicalculator/app/widgets/text/p.dart';
 import 'package:bmicalculator/app/widgets/text/title.dart';
 import 'package:bmicalculator/app/data/sources.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -52,6 +53,10 @@ class Sources extends StatelessWidget {
                 ),
                 subtitle: Text(
                   links[index]['about'],
+                ),
+                trailing: FaIcon(
+                  links[index]['icon'],
+                  color: Colors.indigo,
                 ),
                 onLongPress: () async {
                   await launch(links[index]['url']);
