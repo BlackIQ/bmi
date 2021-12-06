4119a2f835d824fe229de34c0497a823faffbb19
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-06 11:45:15 +0330

    Add icons

--- lib/app/data/sources.dart
+++ lib/app/data/sources.dart
@@ -1,48 +1,58 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 List<Map> links = [
   {
     'site': 'Wikipedia',
     'about': 'Article about Body Mass Index',
+    'icon': FontAwesomeIcons.wikipediaW,
     'url': 'https://en.wikipedia.org/wiki/Body_mass_index',
   },
   {
     'site': 'Wikipedia',
     'about': 'Article about overweight',
+    'icon': FontAwesomeIcons.wikipediaW,
     'url': 'https://en.wikipedia.org/wiki/Overweight',
   },
-  // After
   {
     'site': 'Wikipedia',
     'about': 'Article about Underweight',
+    'icon': FontAwesomeIcons.wikipediaW,
     'url': 'https://en.wikipedia.org/wiki/Underweight',
   },
   {
     'site': 'Wikipedia',
     'about': 'Article about Obesity',
+    'icon': FontAwesomeIcons.wikipediaW,
     'url': 'https://en.wikipedia.org/wiki/Obesity',
   },
   {
     'site': 'Medical News Today',
     'about': 'How much should I weigh for my height and age?',
+    'icon': FontAwesomeIcons.globe,
     'url': 'https://www.medicalnewstoday.com/articles/323446',
   },
   {
     'site': 'Centers for Disease Control and Prevention',
     'about': 'Obesity and adults',
+    'icon': FontAwesomeIcons.globe,
     'url': 'https://www.cdc.gov/obesity/adult/defining.html',
   },
   {
     'site': 'Mount Sinai',
     'about': 'Overweight',
+    'icon': FontAwesomeIcons.globe,
     'url': 'https://www.mountsinai.org/health-library/symptoms/overweight',
   },
   {
     'site': 'WHO',
     'about': 'Obesity and overweight',
+    'icon': FontAwesomeIcons.globe,
     'url': 'https://www.who.int/news-room/fact-sheets/detail/obesity-and-overweight',
   },
   {
     'site': 'NIDDK',
     'about': 'Facts for Adult Overweight & Obesity',
+    'icon': FontAwesomeIcons.globe,
     'url': 'https://www.niddk.nih.gov/health-information/weight-management/adult-overweight-obesity/definition-facts',
   },
 ];
