d696f3c188710de50aa8eb617aac796ac4d04923
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-06 11:36:05 +0330

    Create a button that contains icon

---
+++ lib/app/widgets/button/icon_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CostumeIconButton extends StatelessWidget {
+  CostumeIconButton({required this.submit, required this.text, required this.icon});
+  final submit;
+  final text;
+  final icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: submit,
+      child: Container(
+        height: 45,
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              FaIcon(
+                icon,
+              ),
+              SizedBox(width: 25),
+              Text(
+                text,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
