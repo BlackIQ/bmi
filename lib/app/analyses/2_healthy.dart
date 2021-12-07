7afd25d7f9c900717b81894b3616ac1ea015ec6a
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-07 08:47:01 +0330

    Refactor analyser code

--- lib/app/analyses/2_healthy.dart
+++ lib/app/analyses/2_healthy.dart
@@ -24,15 +24,18 @@ class Healthy extends StatelessWidget {
             Image.asset('assets/healthy.jpg'),
             SizedBox(height: 10),
             ParagraphText(
-              text: "People come in all shapes and sizes. While it’s good to embrace our differences, staying healthy is important for everyone. One key indicator of health is your body fat percentage.",
+              text:
+                  "People come in all shapes and sizes. While it’s good to embrace our differences, staying healthy is important for everyone. One key indicator of health is your body fat percentage.",
             ),
             SizedBox(height: 5),
             ParagraphText(
-                text: "The first step in managing your body fat is to get an accurate measurement of how much body fat you have. The best way gage this is to measure your Body Mass Index (BMI), which estimates your percentage of body fat based on your height and weight.",
+              text:
+                  "The first step in managing your body fat is to get an accurate measurement of how much body fat you have. The best way gage this is to measure your Body Mass Index (BMI), which estimates your percentage of body fat based on your height and weight.",
             ),
             SizedBox(height: 5),
             ParagraphText(
-              text: "For adults, your BMI is calculated by dividing your weight over the square of your height. According to the US Preventive Services Task Force, by factoring in both your height and weight (and not just your weight alone), your BMI helps determine if you are at risk of certain diseases.",
+              text:
+                  "For adults, your BMI is calculated by dividing your weight over the square of your height. According to the US Preventive Services Task Force, by factoring in both your height and weight (and not just your weight alone), your BMI helps determine if you are at risk of certain diseases.",
             ),
             SizedBox(height: 15),
             TitleText(
@@ -40,11 +43,13 @@ class Healthy extends StatelessWidget {
             ),
             SizedBox(height: 10),
             ParagraphText(
-              text: "Adult Body Mass Index is a range of body fat percentages that are determined by your age and height, notes the Centers for Disease Control and Prevention. Typically, people with a BMI between 18.5 and 25 are considered at a healthy percentile. Those with a BMI lower than 18.5 are considered underweight, and those with a BMI between 25 and 30 are considered overweight. People with a BMI of 30 or higher are considered obese and at high risk for disease.",
+              text:
+                  "Adult Body Mass Index is a range of body fat percentages that are determined by your age and height, notes the Centers for Disease Control and Prevention. Typically, people with a BMI between 18.5 and 25 are considered at a healthy percentile. Those with a BMI lower than 18.5 are considered underweight, and those with a BMI between 25 and 30 are considered overweight. People with a BMI of 30 or higher are considered obese and at high risk for disease.",
             ),
             SizedBox(height: 5),
             ParagraphText(
-              text: "While BMI measurements are standard for most people, there are occasional exceptions to the rule. For example, athletes and people with a higher level of muscle mass may generate a BMI calculation that designates them as overweight or even obese, despite their high level of fitness. It’s important for athletes to talk to their doctor to ensure they’re maintaining a healthy body fat balance.",
+              text:
+                  "While BMI measurements are standard for most people, there are occasional exceptions to the rule. For example, athletes and people with a higher level of muscle mass may generate a BMI calculation that designates them as overweight or even obese, despite their high level of fitness. It’s important for athletes to talk to their doctor to ensure they’re maintaining a healthy body fat balance.",
             ),
             SizedBox(height: 15),
             TitleText(
@@ -52,7 +57,8 @@ class Healthy extends StatelessWidget {
             ),
             SizedBox(height: 10),
             ParagraphText(
-              text: "When it comes to the amount of body fat you carry, the stakes are high. Obesity and high levels of body fat carry a variety of health consequences, and can affect your overall health and quality of life. They include:",
+              text:
+                  "When it comes to the amount of body fat you carry, the stakes are high. Obesity and high levels of body fat carry a variety of health consequences, and can affect your overall health and quality of life. They include:",
             ),
             SizedBox(height: 5),
             ParagraphText(text: "- High blood pressure"),
@@ -75,7 +81,8 @@ class Healthy extends StatelessWidget {
             SizedBox(height: 2),
             ParagraphText(text: "- Certain types of cancer"),
             SizedBox(height: 2),
-            ParagraphText(text: "- Mental disorders such as anxiety and depression"),
+            ParagraphText(
+                text: "- Mental disorders such as anxiety and depression"),
             SizedBox(height: 2),
             ParagraphText(text: "- Increased risk of death"),
             SizedBox(height: 15),
@@ -84,7 +91,8 @@ class Healthy extends StatelessWidget {
             ),
             SizedBox(height: 10),
             ParagraphText(
-              text: "Maintaining a healthy BMI takes some work. For starters, it’s important to exercise at least 60-90 minutes most days of the week. Staying hydrated and eat a balanced diet are equally important. If your BMI isn’t where it should be, you may want to reduce your caloric intake and increase your exercise.",
+              text:
+                  "Maintaining a healthy BMI takes some work. For starters, it’s important to exercise at least 60-90 minutes most days of the week. Staying hydrated and eat a balanced diet are equally important. If your BMI isn’t where it should be, you may want to reduce your caloric intake and increase your exercise.",
             ),
             SizedBox(height: 15),
             TitleText(
@@ -92,7 +100,8 @@ class Healthy extends StatelessWidget {
             ),
             SizedBox(height: 10),
             ParagraphText(
-              text: "If your BMI score is outside of a healthy percentile, it’s time to call a professional. Consult your primary care doctor for advice on ways you can achieve a healthy balance of body fat. If needed, you may need to see a dietician to achieve your health goals.",
+              text:
+                  "If your BMI score is outside of a healthy percentile, it’s time to call a professional. Consult your primary care doctor for advice on ways you can achieve a healthy balance of body fat. If needed, you may need to see a dietician to achieve your health goals.",
             ),
           ],
         ),
--- lib/app/analyses/3_overweight.dart
+++ lib/app/analyses/3_overweight.dart
@@ -28,24 +28,27 @@ class OverWight extends StatelessWidget {
             ),
             SizedBox(height: 10),
             ParagraphText(
-              text: "Being overweight or fat is having more body fat than is optimally healthy. Being overweight is especially common where food supplies are plentiful and lifestyles are sedentary.",
+              text:
+                  "Being overweight or fat is having more body fat than is optimally healthy. Being overweight is especially common where food supplies are plentiful and lifestyles are sedentary.",
             ),
             SizedBox(height: 5),
             ParagraphText(
-              text: "As of 2003, excess weight reached epidemic proportions globally, with more than 1 billion adults being either overweight or obese. In 2013, this increased to more than 2 billion. Increases have been observed across all age groups.",
+              text:
+                  "As of 2003, excess weight reached epidemic proportions globally, with more than 1 billion adults being either overweight or obese. In 2013, this increased to more than 2 billion. Increases have been observed across all age groups.",
             ),
             SizedBox(height: 5),
             ParagraphText(
-              text: "A healthy body requires a minimum amount of fat for proper functioning of the hormonal, reproductive, and immune systems, as thermal insulation, as shock absorption for sensitive areas, and as energy for future use; however, the accumulation of too much storage fat can impair movement, flexibility, and alter the appearance of the body.",
+              text:
+                  "A healthy body requires a minimum amount of fat for proper functioning of the hormonal, reproductive, and immune systems, as thermal insulation, as shock absorption for sensitive areas, and as energy for future use; however, the accumulation of too much storage fat can impair movement, flexibility, and alter the appearance of the body.",
             ),
             SizedBox(height: 15),
             TitleText(
-              text:
-              "Classification",
+              text: "Classification",
             ),
             SizedBox(height: 10),
             ParagraphText(
-              text: "The degree to which a person is overweight is generally described by the body mass index (BMI). Overweight is defined as a BMI of 25 or more, thus it includes pre-obesity defined as a BMI between 25 and 29.9 and obesity as defined by a BMI of 30 or more.[4][5] Pre-obese and overweight however are often used interchangeably, thus giving overweight a common definition of a BMI of between 25 and 29.9. There are, however, several other common ways to measure the amount of adiposity or fat present in an individual's body.",
+              text:
+                  "The degree to which a person is overweight is generally described by the body mass index (BMI). Overweight is defined as a BMI of 25 or more, thus it includes pre-obesity defined as a BMI between 25 and 29.9 and obesity as defined by a BMI of 30 or more.[4][5] Pre-obese and overweight however are often used interchangeably, thus giving overweight a common definition of a BMI of between 25 and 29.9. There are, however, several other common ways to measure the amount of adiposity or fat present in an individual's body.",
             ),
             SizedBox(height: 15),
             TitleText(
@@ -53,32 +56,37 @@ class OverWight extends StatelessWidget {
             ),
             SizedBox(height: 10),
             ParagraphText(
-              text: 'While the negative health outcomes associated with obesity are accepted within the medical community, the health implications of the overweight category are more controversial. The generally accepted view is that being overweight causes similar health problems to obesity, but to a lesser degree. A 2016 review estimated that the risk of death increases by seven percent among overweight people with a BMI of 25 to 27.5 and 20 percent among overweight people with a BMI of 27.5 to 30. The Framingham heart study found that being overweight at age 40 reduced life expectancy by three years. Being overweight also increases the risk of oligospermia and azoospermia in men.',
+              text:
+                  'While the negative health outcomes associated with obesity are accepted within the medical community, the health implications of the overweight category are more controversial. The generally accepted view is that being overweight causes similar health problems to obesity, but to a lesser degree. A 2016 review estimated that the risk of death increases by seven percent among overweight people with a BMI of 25 to 27.5 and 20 percent among overweight people with a BMI of 27.5 to 30. The Framingham heart study found that being overweight at age 40 reduced life expectancy by three years. Being overweight also increases the risk of oligospermia and azoospermia in men.',
             ),
             SizedBox(height: 5),
             ParagraphText(
-              text: 'Katherine Flegal et al., however, found that the mortality rate for individuals who are classified as overweight (BMI 25 to 29.9) may actually be lower than for those with an "ideal" weight (BMI 18.5 to 24.9), noting that many studies show that the lowest mortality rate is at a BMI close to 25.',
+              text:
+                  'Katherine Flegal et al., however, found that the mortality rate for individuals who are classified as overweight (BMI 25 to 29.9) may actually be lower than for those with an "ideal" weight (BMI 18.5 to 24.9), noting that many studies show that the lowest mortality rate is at a BMI close to 25.',
             ),
             SizedBox(height: 5),
             ParagraphText(
-              text: 'Being overweight has been identified as a cause of cancer, and is projected to overtake smoking as the primary cause of cancer in developed countries as cases of smoking-related cancer dwindle.',
+              text:
+                  'Being overweight has been identified as a cause of cancer, and is projected to overtake smoking as the primary cause of cancer in developed countries as cases of smoking-related cancer dwindle.',
             ),
             SizedBox(height: 5),
             ParagraphText(
-              text: 'Psychological well-being is also at risk in the overweight individual due to social discrimination. However, children under the age of eight are normally not affected.',
+              text:
+                  'Psychological well-being is also at risk in the overweight individual due to social discrimination. However, children under the age of eight are normally not affected.',
             ),
             SizedBox(height: 5),
             ParagraphText(
-              text: 'Being overweight has been shown not to increase mortality[qualify evidence] in older people: in a study of 70 to 75-year old Australians, mortality was lowest for "overweight" individuals (BMI 25 to 29.9), while a study of Koreans found that, among those initially aged 65 or more, an increase in BMI to above 25 was not associated with increased risk of death.',
+              text:
+                  'Being overweight has been shown not to increase mortality[qualify evidence] in older people: in a study of 70 to 75-year old Australians, mortality was lowest for "overweight" individuals (BMI 25 to 29.9), while a study of Koreans found that, among those initially aged 65 or more, an increase in BMI to above 25 was not associated with increased risk of death.',
             ),
             SizedBox(height: 15),
             TitleText(
-              text:
-              "Classification",
+              text: "Classification",
             ),
             SizedBox(height: 10),
             ParagraphText(
-              text: "The degree to which a person is overweight is generally described by the body mass index (BMI). Overweight is defined as a BMI of 25 or more, thus it includes pre-obesity defined as a BMI between 25 and 29.9 and obesity as defined by a BMI of 30 or more.[4][5] Pre-obese and overweight however are often used interchangeably, thus giving overweight a common definition of a BMI of between 25 and 29.9. There are, however, several other common ways to measure the amount of adiposity or fat present in an individual's body.",
+              text:
+                  "The degree to which a person is overweight is generally described by the body mass index (BMI). Overweight is defined as a BMI of 25 or more, thus it includes pre-obesity defined as a BMI between 25 and 29.9 and obesity as defined by a BMI of 30 or more.[4][5] Pre-obese and overweight however are often used interchangeably, thus giving overweight a common definition of a BMI of between 25 and 29.9. There are, however, several other common ways to measure the amount of adiposity or fat present in an individual's body.",
             ),
             SizedBox(height: 15),
             TitleText(
@@ -86,7 +94,8 @@ class OverWight extends StatelessWidget {
             ),
             SizedBox(height: 10),
             ParagraphText(
-              text: 'Being overweight is generally caused by the intake of more calories (by eating) than are expended by the body (by exercise and everyday activity). Factors that may contribute to this imbalance include:',
+              text:
+                  'Being overweight is generally caused by the intake of more calories (by eating) than are expended by the body (by exercise and everyday activity). Factors that may contribute to this imbalance include:',
             ),
             SizedBox(height: 5),
             ParagraphText(
@@ -94,43 +103,44 @@ class OverWight extends StatelessWidget {
             ),
             SizedBox(height: 2),
             ParagraphText(
-                text: '- Alcoholism',
+              text: '- Alcoholism',
             ),
             SizedBox(height: 2),
             ParagraphText(
-                text: '- Genetic predisposition',
+              text: '- Genetic predisposition',
             ),
             SizedBox(height: 2),
             ParagraphText(
-                text: '- Hormonal imbalances (e.g. hypothyroidism)',
+              text: '- Hormonal imbalances (e.g. hypothyroidism)',
             ),
             SizedBox(height: 2),
             ParagraphText(
-                text: '- Insufficient or poor-quality sleep',
+              text: '- Insufficient or poor-quality sleep',
             ),
             SizedBox(height: 2),
             ParagraphText(
-                text: '- Limited physical exercise and a sedentary lifestyle',
+              text: '- Limited physical exercise and a sedentary lifestyle',
             ),
             SizedBox(height: 2),
             ParagraphText(
-                text: '- Poor nutrition',
+              text: '- Poor nutrition',
             ),
             SizedBox(height: 2),
             ParagraphText(
-                text: '- Metabolic disorders, which could be caused by repeated attempts to lose weight by weight cycling',
+              text:
+                  '- Metabolic disorders, which could be caused by repeated attempts to lose weight by weight cycling',
             ),
             SizedBox(height: 2),
             ParagraphText(
-                text: '- Overeating',
+              text: '- Overeating',
             ),
             SizedBox(height: 2),
             ParagraphText(
-                text: '- Psychotropic medication (e.g. olanzapine)',
+              text: '- Psychotropic medication (e.g. olanzapine)',
             ),
             SizedBox(height: 2),
             ParagraphText(
-                text: '- Smoking cessation and other stimulant withdrawal',
+              text: '- Smoking cessation and other stimulant withdrawal',
             ),
             SizedBox(height: 2),
             ParagraphText(
@@ -138,41 +148,46 @@ class OverWight extends StatelessWidget {
             ),
             SizedBox(height: 5),
             ParagraphText(
-              text: 'People who have insulin dependent diabetes and chronically overdose insulin may gain weight, while people who already are overweight may develop insulin tolerance, and in the long run develop type II diabetes.',
+              text:
+                  'People who have insulin dependent diabetes and chronically overdose insulin may gain weight, while people who already are overweight may develop insulin tolerance, and in the long run develop type II diabetes.',
             ),
             SizedBox(height: 15),
             TitleText(
-              text:
-              "Treatment",
+              text: "Treatment",
             ),
             SizedBox(height: 10),
             ParagraphText(
-              text: "The usual treatments for overweight individuals is diet and physical exercise.",
+              text:
+                  "The usual treatments for overweight individuals is diet and physical exercise.",
             ),
             SizedBox(height: 5),
             ParagraphText(
-              text: "Dietitians generally recommend eating several balanced meals dispersed through the day, with a combination of progressive, primarily aerobic, physical exercise. In fact, some research found benefits from physical activity, diet and behaviour changes on BMI in children from 12 to 17 years old.",
+              text:
+                  "Dietitians generally recommend eating several balanced meals dispersed through the day, with a combination of progressive, primarily aerobic, physical exercise. In fact, some research found benefits from physical activity, diet and behaviour changes on BMI in children from 12 to 17 years old.",
             ),
             SizedBox(height: 5),
             ParagraphText(
-              text: "Because these general treatments help most case of obesity, they are common in all levels of overweight individuals.",
+              text:
+                  "Because these general treatments help most case of obesity, they are common in all levels of overweight individuals.",
             ),
             SizedBox(height: 5),
             ParagraphText(
-              text: "Considering that most of the treatment strategies are directed to change lifestyle-related behaviours of individuals (namely in dietary and physical activity), the transtheoretical model (TTM) has been used as a framework to design weight management interventions. A systematic review assessed the effectiveness of dietary and physical activity interventions based on the TTM in producing sustainable (one year or longer) weight loss in overweight and obese adults. The included studies did not allow to produce conclusive evidence about the impact of the use of this model combined with these interventions on sustainable weight loss. Nevertheless, very low quality scientific evidence suggests that this approach may lead to improvements in physical activity and dietary habits, namely increased in both exercise duration and frequency, and fruits and vegetables consumption, alongside with reduced dietary fat intake.",
+              text:
+                  "Considering that most of the treatment strategies are directed to change lifestyle-related behaviours of individuals (namely in dietary and physical activity), the transtheoretical model (TTM) has been used as a framework to design weight management interventions. A systematic review assessed the effectiveness of dietary and physical activity interventions based on the TTM in producing sustainable (one year or longer) weight loss in overweight and obese adults. The included studies did not allow to produce conclusive evidence about the impact of the use of this model combined with these interventions on sustainable weight loss. Nevertheless, very low quality scientific evidence suggests that this approach may lead to improvements in physical activity and dietary habits, namely increased in both exercise duration and frequency, and fruits and vegetables consumption, alongside with reduced dietary fat intake.",
             ),
             SizedBox(height: 15),
             TitleText(
-              text:
-              "Epidemiology",
+              text: "Epidemiology",
             ),
             SizedBox(height: 10),
             ParagraphText(
-              text: "The World Health Organization (WHO) estimated that nearly 2 billion adults worldwide, aged 18 years and older, were overweight in 2016.",
+              text:
+                  "The World Health Organization (WHO) estimated that nearly 2 billion adults worldwide, aged 18 years and older, were overweight in 2016.",
             ),
             SizedBox(height: 5),
             ParagraphText(
-              text: "As much as 71.6% of the United States' adult population aged 20 and over is considered either overweight or obese, and this percentage has increased over the last four decades.",
+              text:
+                  "As much as 71.6% of the United States' adult population aged 20 and over is considered either overweight or obese, and this percentage has increased over the last four decades.",
             ),
           ],
         ),
