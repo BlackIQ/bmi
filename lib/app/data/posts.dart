4589913ff0c05a5837dc89aa74519069b0c96f6d
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-07 09:28:57 +0330

    Add new articles and update icons

--- lib/app/data/posts.dart
+++ lib/app/data/posts.dart
@@ -1,6 +1,9 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 List<Map> links = [
   {
     'row': 1,
+    'icon': FontAwesomeIcons.signal,
     'site': 'Health x Change',
     'about': '10 Foods for the Growing Child?',
     'url':
@@ -8,6 +11,7 @@ List<Map> links = [
   },
   {
     'row': 2,
+    'icon': FontAwesomeIcons.solidSmile,
     'site': 'Healthy children',
     'about': 'Selecting Snacks for Toddlers',
     'url':
@@ -15,8 +19,23 @@ List<Map> links = [
   },
   {
     'row': 3,
-    'site': 'Web MD',
+    'icon': FontAwesomeIcons.fish,
+    'site': ' MD',
     'about': 'Selecting Snacks for Toddlers',
     'url': 'https://www.webmd.com/diet/features/estimated-calorie-requirement',
+  },
+  {
+    'row': 4,
+    'site': 'NewYork Times',
+    'icon': FontAwesomeIcons.apple,
+    'about': 'Health section',
+    'url': 'https://www.nytimes.com/international/section/health',
+  },
+  {
+    'row': 5,
+    'site': 'PMC Labs',
+    'icon': FontAwesomeIcons.hamburger,
+    'about': 'The Epidemiology of Obesity: A Big Picture',
+    'url': 'https://www.ncbi.nlm.nih.gov/labs/pmc/articles/PMC4859313/',
   },
 ];
