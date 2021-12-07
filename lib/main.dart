d97e0629f187ed409559c56204a825118cc2ddeb
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-07 17:11:48 +0330

    Add SplashScreen and ensureInitialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,35 @@
 import 'package:bmicalculator/app/index.dart';
+import 'package:bmicalculator/app/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        textTheme: GoogleFonts.nunitoTextTheme(),
-      ),
-      debugShowCheckedModeBanner: false,
-      title: 'Body Analyser',
-      home: BmiCalculator(),
+    return FutureBuilder(
+      future: Init.instance.initialize(),
+      builder: (context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(
+            home: SplashScreen(),
+          );
+        } else {
+          return MaterialApp(
+            theme: ThemeData(
+              primarySwatch: Colors.indigo,
+              textTheme: GoogleFonts.nunitoTextTheme(),
+            ),
+            debugShowCheckedModeBanner: false,
+            title: 'Body Analyser',
+            home: BmiCalculator(),
+          );
+        }
+      },
     );
   }
 }
