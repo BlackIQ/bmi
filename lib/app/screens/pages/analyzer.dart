ab4be33e2ac6a8259ca1aedab34fdc11c9765f8f
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-15 23:01:55 +0330

    Add analyser page in pages dir

---
+++ lib/app/screens/pages/analyzer.dart
@@ -0,0 +1,58 @@
+import 'package:bmicalculator/app/screens/pages/1_underweight.dart';
+import 'package:bmicalculator/app/screens/pages/2_healthy.dart';
+import 'package:bmicalculator/app/screens/pages/3_overweight.dart';
+import 'package:bmicalculator/app/screens/pages/4_obesity.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class BmiGetRange extends StatefulWidget {
+  BmiGetRange({@required this.bmi});
+
+  final bmi;
+
+  @override
+  _BmiGetRangeState createState() => _BmiGetRangeState();
+}
+
+class _BmiGetRangeState extends State<BmiGetRange> {
+  @override
+  Widget build(BuildContext context) {
+    final bmi = widget.bmi;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Analyse BMI : ${widget.bmi}',
+          style: GoogleFonts.nunito(),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      backgroundColor: Colors.white,
+      body: _build(bmi),
+    );
+  }
+
+  Widget _build(String bmi) {
+    int _bmi = int.parse(bmi);
+    if (_bmi <= 18.5) {
+      return UnderWeight();
+    } else if (_bmi >= 18.5 && _bmi <= 24.9) {
+      return Healthy();
+    } else if (_bmi >= 25 && _bmi <= 29.9) {
+      return OverWight();
+    } else if (_bmi >= 30) {
+      return Obesity();
+    } else {
+      return Center(
+        child: Text(
+          'Invalid BMI',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+            color: Colors.indigo,
+          ),
+        ),
+      );
+    }
+  }
+}
