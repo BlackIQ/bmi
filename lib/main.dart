54bf39b38f27ab17f4c5599cce998fb65cb8cc69
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-16 01:02:23 +0330

    Create a condition for user home and splash

--- lib/main.dart
+++ lib/main.dart
@@ -14,27 +14,16 @@ class MyApp extends StatelessWidget {
     return FutureBuilder(
       future: Init.instance.initialize(),
       builder: (context, AsyncSnapshot snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return MaterialApp(
-            theme: ThemeData(
-              primarySwatch: Colors.indigo,
-              textTheme: GoogleFonts.nunitoTextTheme(),
-            ),
-            debugShowCheckedModeBanner: false,
-            title: 'Body Analyser SplashScreen',
-            home: SplashScreen(),
-          );
-        } else {
-          return MaterialApp(
-            theme: ThemeData(
-              primarySwatch: Colors.indigo,
-              textTheme: GoogleFonts.nunitoTextTheme(),
-            ),
-            debugShowCheckedModeBanner: false,
-            title: 'Body Analyser',
-            home: Home(),
-          );
-        }
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.indigo,
+            textTheme: GoogleFonts.nunitoTextTheme(),
+          ),
+          home: snapshot.connectionState == ConnectionState.waiting
+              ? SplashScreen()
+              : Home(),
+        );
       },
     );
   }
