47c16d0bc45aa39f30ad82a3bc4ba0fe66806ee4
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-17 00:41:20 +0330

    Change fonts icon

--- lib/app/services/posts.dart
+++ lib/app/services/posts.dart
@@ -3,7 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 List<Map> links = [
   {
     'row': 1,
-    'icon': FontAwesomeIcons.signal,
+    'icon': 'signal',
     'site': 'Health x Change',
     'about': '10 Foods for the Growing Child?',
     'url':
@@ -11,7 +11,7 @@ List<Map> links = [
   },
   {
     'row': 2,
-    'icon': FontAwesomeIcons.solidSmile,
+    'icon': 'solidSmile',
     'site': 'Healthy children',
     'about': 'Selecting Snacks for Toddlers',
     'url':
@@ -19,7 +19,7 @@ List<Map> links = [
   },
   {
     'row': 3,
-    'icon': FontAwesomeIcons.fish,
+    'icon': 'fish',
     'site': ' MD',
     'about': 'Selecting Snacks for Toddlers',
     'url': 'https://www.webmd.com/diet/features/estimated-calorie-requirement',
@@ -27,50 +27,52 @@ List<Map> links = [
   {
     'row': 4,
     'site': 'NewYork Times',
-    'icon': FontAwesomeIcons.apple,
+    'icon': 'apple',
     'about': 'Health section',
     'url': 'https://www.nytimes.com/international/section/health',
   },
   {
     'row': 5,
     'site': 'PMC Labs',
-    'icon': FontAwesomeIcons.hamburger,
+    'icon': 'hamburger',
     'about': 'The Epidemiology of Obesity: A Big Picture',
     'url': 'https://www.ncbi.nlm.nih.gov/labs/pmc/articles/PMC4859313/',
   },
   {
     'row': 6,
     'site': 'Medicine Net',
-    'icon': FontAwesomeIcons.question,
+    'icon': 'question',
     'about': "Why Is a Child's Health Important?",
     'url': 'https://www.medicinenet.com/childrens_health/article.htm',
   },
   {
     'row': 7,
     'site': 'PMC Labs',
-    'icon': FontAwesomeIcons.userAlt,
+    'icon': 'userAlt',
     'about': 'A healthy old age: realistic or futile goal?',
     'url': 'https://www.ncbi.nlm.nih.gov/labs/pmc/articles/PMC1118909/',
   },
   {
     'row': 8,
     'site': 'US News',
-    'icon': FontAwesomeIcons.globeAmericas,
+    'icon': 'globeAmericas',
     'about': 'Obesity in America: A Public Health Crisis',
-    'url': 'https://www.usnews.com/news/healthiest-communities/articles/2019-09-19/obesity-in-america-a-guide-to-the-public-health-crisis',
+    'url':
+        'https://www.usnews.com/news/healthiest-communities/articles/2019-09-19/obesity-in-america-a-guide-to-the-public-health-crisis',
   },
   {
     'row': 9,
     'site': 'Kids Health',
-    'icon': FontAwesomeIcons.child,
+    'icon': 'child',
     'about': 'When Being Overweight Is a Health Problem',
     'url': 'https://kidshealth.org/en/teens/obesity.html',
   },
   {
     'row': 10,
     'site': 'Better Health',
-    'icon': FontAwesomeIcons.laptop,
+    'icon': 'laptop',
     'about': 'Computer-related injuries',
-    'url': 'https://www.betterhealth.vic.gov.au/health/healthyliving/computer-related-injuries',
+    'url':
+        'https://www.betterhealth.vic.gov.au/health/healthyliving/computer-related-injuries',
   },
 ];
