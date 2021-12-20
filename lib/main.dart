baf378ad7f20224fa04ec8106db5e90186639137
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-12-20 11:45:02 +0330

    Add ProviderStream

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
-import 'package:bmicalculator/app/screens/home.dart';
-import 'package:bmicalculator/app/screens/splash/splash_screen.dart';
+import 'package:bmicalculator/app/models/user.dart';
+import 'package:bmicalculator/app/screens/home/splash/splash_screen.dart';
+import 'package:bmicalculator/app/screens/landing.dart';
+import 'package:bmicalculator/app/services/authentication.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,20 +14,23 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: Init.instance.initialize(),
-      builder: (context, AsyncSnapshot snapshot) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            primarySwatch: Colors.indigo,
-            textTheme: GoogleFonts.nunitoTextTheme(),
-          ),
-          home: snapshot.connectionState == ConnectionState.waiting
-              ? SplashScreen()
-              : Home(),
-        );
-      },
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: FutureBuilder(
+        future: Init.instance.initialize(),
+        builder: (context, AsyncSnapshot snapshot) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primarySwatch: Colors.indigo,
+              textTheme: GoogleFonts.nunitoTextTheme(),
+            ),
+            home: snapshot.connectionState == ConnectionState.waiting
+                ? SplashScreen()
+                : Landing(),
+          );
+        },
+      ),
     );
   }
 }
